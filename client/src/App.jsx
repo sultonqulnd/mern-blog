6083e0cb440760fcbcc6f535761a879013d8236f
sultonqulnd <sultonqulnd@gmail.com>
2024-02-04 19:08:14 +0500

    header done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">MERN Blog</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
