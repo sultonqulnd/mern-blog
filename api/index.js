2340c33e7aef1fc650ec39651f459b83d7d5c242
sultonqulnd <sultonqulnd@gmail.com>
2024-02-22 10:26:16 +0500

    complete create a post api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
