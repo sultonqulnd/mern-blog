b207c148d52f7483c604ec8862c847342164f11a
sultonqulnd <sultonqulnd@gmail.com>
2024-02-27 11:14:38 +0500

    create get posts api route

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 const DashPosts = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return <div>DashPosts</div>;
 };
 
