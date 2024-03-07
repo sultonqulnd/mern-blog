96fa4629889c5200b20a83de90e98cc3151998f2
sultonqulnd <sultonqulnd@gmail.com>
2024-03-07 18:27:33 +0500

    some changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'mernblog-snd.firebaseapp.com',
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: '1:500890446757:web:1c63284c0beb48f8a1164c',
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
