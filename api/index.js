09cb0c88afa452f305d421f2d1dcc44db13617d3
sultonqulnd <sultonqulnd@gmail.com>
2024-03-11 17:46:18 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB is connected'))
+  .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -20,17 +23,23 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
